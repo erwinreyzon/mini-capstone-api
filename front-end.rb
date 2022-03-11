61bc53ca697e7dc94f737ae456571734b64c2d5e
erwin <winzon353535@gmail.com>
2022-03-11 12:21:41 -0800

    started front end create function

--- front-end.rb
+++ front-end.rb
@@ -1,5 +1,25 @@
 require "http"
+require "/app/controllers/products.controller.rb"
 
 response = HTTP.get("http://localhost:3000/products")
 
-pp response.parse(:json)
+# pp response.parse(:json)
+
+# Create (not working)
+
+puts "Please input product name:"
+name = gets.chomp 
+puts "Please input product price:"
+price = gets.chomp
+price = price.to_i
+puts "Please input product image URL:"
+image_url = gets.chomp
+puts "Please input product description:"
+description = gets.chomp
+
+{
+  "name": name,
+  "price": price,
+  "image_url": image_url,
+  "description": description
+}
