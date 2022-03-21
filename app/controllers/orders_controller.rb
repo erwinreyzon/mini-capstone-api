ac1fe38e25e347de496eb84f58129cc06db27f27
erwin <winzon353535@gmail.com>
2022-03-21 12:31:03 -0700

    updating create, needs fix

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -10,17 +10,19 @@ class OrdersController < ApplicationController
     render json: order.as_json
   end
 
-  def create
-    order = Order.new(
-    user_id: current_user.id,
-    product_id: params["product_id"],
-    quantity: params["quantity"],
-    subtotal: params["subtotal"],
-    tax: params["tax"],
-    total: params["total"]
-    )
-    order.save
-    render json: order.as_json
-  end
+  #fix
+  # def create
+  #   product = Product.find_by(id: params["product_id"])
+  #   order = Order.new(
+  #   user_id: current_user.id,
+  #   product_id: params["product_id"],
+  #   quantity: params["quantity"],
+  #   subtotal: params["subtotal"],
+  #   tax: product.tax,
+  #   total: product.total
+  #   )
+  #   order.save
+  #   render json: order.as_json
+  # end
 
 end
