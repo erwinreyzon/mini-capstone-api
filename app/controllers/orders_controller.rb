778d5ca6536f0fa8047a1e05df062d0171b30b3e
erwin <winzon353535@gmail.com>
2022-03-22 10:33:45 -0700

    update order create with correct pricing math

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -10,19 +10,21 @@ class OrdersController < ApplicationController
     render json: order.as_json
   end
 
-  #fix
-  # def create
-  #   product = Product.find_by(id: params["product_id"])
-  #   order = Order.new(
-  #   user_id: current_user.id,
-  #   product_id: params["product_id"],
-  #   quantity: params["quantity"],
-  #   subtotal: params["subtotal"],
-  #   tax: product.tax,
-  #   total: product.total
-  #   )
-  #   order.save
-  #   render json: order.as_json
-  # end
+  def create
+    product = Product.find_by(id: params["product_id"])
+    calculated_subtotal = product.price * params[:quantity]
+    calculated_tax = calculated_subtotal * 0.09
+    calculated_total = calculated_subtotal + calculated_tax
+    order = Order.new(
+    user_id: current_user.id,
+    product_id: params["product_id"],
+    quantity: params["quantity"],
+    subtotal: calculated_subtotal,
+    tax: calculated_tax,
+    total: calculated_total
+    )
+    order.save
+    render json: order.as_json
+  end
 
 end
