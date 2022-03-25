f91ac6b1677005ea9bafe288c4f253fe9b38a95b
erwin <winzon353535@gmail.com>
2022-03-25 12:12:23 -0700

    adds associations between models carted_products, user, orders, and products

--- app/models/product.rb
+++ app/models/product.rb
@@ -13,6 +13,7 @@ class Product < ApplicationRecord
   has_many :orders
   has_many :category_products
   has_many :categories, through: :category_products
+  has_many :carted_products
 
   def is_discounted?
     price <= 10
