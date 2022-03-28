5b4af404876e0004ca78844b8f92749c070dcac1
erwin <winzon353535@gmail.com>
2022-03-28 09:41:16 -0700

    updates carted_products controller create user_id, status, and order_id

--- app/controllers/carted_products_controller.rb
+++ app/controllers/carted_products_controller.rb
@@ -5,8 +5,8 @@ class CartedProductsController < ApplicationController
       user_id: current_user.id,
       product_id: params["product_id"],
       quantity: params["quantity"],
-      status: params["status"],
-      order_id: params["order_id"]
+      status: "carted",
+      order_id: nil
     )
     carted_product.save
     render json: carted_product
