ed102049f0750a87ab5f3bd1c8d3f64a0a5678a9
erwin <winzon353535@gmail.com>
2022-03-28 09:32:20 -0700

    tests carted_products controller action create, first draft

--- app/controllers/carted_products_controller.rb
+++ app/controllers/carted_products_controller.rb
@@ -1,12 +1,14 @@
 class CartedProductsController < ApplicationController
 
   def create
-    CartedProduct.create(
-      user_id: params["user_id"],
+    carted_product = CartedProduct.new(
+      user_id: current_user.id,
       product_id: params["product_id"],
       quantity: params["quantity"],
       status: params["status"],
       order_id: params["order_id"]
     )
+    carted_product.save
+    render json: carted_product
   end
 end
