c3cdab2eda783595858f856b79bce6eb53b7f8f7
erwin <winzon353535@gmail.com>
2022-03-28 12:16:30 -0700

    add views index for carted products

--- app/controllers/carted_products_controller.rb
+++ app/controllers/carted_products_controller.rb
@@ -1,18 +1,18 @@
 class CartedProductsController < ApplicationController
 
   def index
-    carted_products = current_user.carted_products.where(status: "carted")
-    render json: carted_products
+    @carted_products = current_user.carted_products.where(status: "carted")
+    render template: "carted_products/index"
   end
 
   def create
-    carted_product = CartedProduct.new(
+    @carted_product = CartedProduct.new(
       user_id: current_user.id,
       product_id: params["product_id"],
       quantity: params["quantity"],
       status: "carted"
     )
-    carted_product.save
+    @carted_product.save
     render json: carted_product
   end
 
